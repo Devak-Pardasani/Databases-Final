8a7519189744dce5262012831e494c1b5c11bf54
jlai682 <jasonlai682@gmail.com>
2025-12-03 15:39:09 -0500

    Movie Endpoints

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Movies
 from .serializers import MovieSerializer
 
-@api_view(['GET'])
-def get_movies(request):
-    movies = Movies.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+@api_view(["GET", "POST"])
+def movies_list(request):
 
+    # GET → return all movies
+    if request.method == "GET":
+        movies = Movies.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    # POST → create a new movie
+    if request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- backend/server/urls.py
+++ backend/server/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from api.views import get_movies
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("movies/", get_movies),
+    path("api/", include("api.urls")),
 ]
